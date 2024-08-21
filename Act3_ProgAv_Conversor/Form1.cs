ebb961fb669991a832791f6d1fd112b703cc35c1
brianaquino <brian.aquino50@unach.mx>
2024-08-20 21:55:00 -0600

    Agregar archivos de proyecto.

---
+++ Act3_ProgAv_Conversor/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act3_ProgAv_Conversor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float Celsius = float.Parse(textBox1.Text);
+            float Fahrenheit = (Celsius - 32) * 5f / 9f;
+            textBox2.Text = Fahrenheit.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float Fahrenheit = float.Parse(textBox2.Text);
+            float Celsius = (Fahrenheit * 9f / 5f) + 32;
+            textBox1.Text = Celsius.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
